dc216faafcfec1d54efc59bc757a25ff7fa843dd
unknown <bhimpdsharma@gmail.com>
2025-06-01 11:06:04 +0545

    block comment added

--- changelog/20250601_105004_proc_count.sql
+++ changelog/20250601_105004_proc_count.sql
@@ -8,9 +8,8 @@ BEGIN
   SELECT 'Number of accounts in DB:', COUNT(*) FROM mysql.user;
 END;
 
---rollback 
-/* DROP PROCEDURE IF EXISTS account_count;
- CREATE PROCEDURE account_count()
- BEGIN
- SELECT 'Number of accounts:', COUNT(*) FROM mysql.user;
- END;*/
+--rollback DROP PROCEDURE IF EXISTS account_count;
+--rollback CREATE PROCEDURE account_count()
+--rollback BEGIN
+--rollback  SELECT 'Number of accounts:', COUNT(*) FROM mysql.user;
+--rollback END;
