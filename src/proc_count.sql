38f1eaea79823b676e155f6202c502be242eae87
unknown <bhimpdsharma@gmail.com>
2025-06-01 10:13:21 +0545

    update rollback

--- src/proc_count.sql
+++ src/proc_count.sql
@@ -8,10 +8,9 @@ BEGIN
   SELECT 'Number of accounts in DB:', COUNT(*) FROM mysql.user;
 END;
 
---rollback /*
+--rollback
 DROP PROCEDURE IF EXISTS account_count;
 CREATE PROCEDURE account_count()
 BEGIN
   SELECT 'Number of accounts:', COUNT(*) FROM mysql.user;
 END;
-*/
