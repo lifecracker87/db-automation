5b55e753468c8f99aac7e00a07deb2ed999295f7
unknown <bhimpdsharma@gmail.com>
2025-06-01 10:00:49 +0545

    procedure change

--- src/proc_count.sql
+++ src/proc_count.sql
@@ -1,10 +1,17 @@
 --liquibase formatted sql
 
---changeset bhim.sharma:Create Procedure User Count
+--changeset bhim.sharma:Modify Procedure User Count
 
+DROP PROCEDURE IF EXISTS account_count;
+CREATE PROCEDURE account_count()
+BEGIN
+  SELECT 'Number of accounts in DB:', COUNT(*) FROM mysql.user;
+END;
+
+--rollback /*
+DROP PROCEDURE IF EXISTS account_count;
 CREATE PROCEDURE account_count()
 BEGIN
   SELECT 'Number of accounts:', COUNT(*) FROM mysql.user;
 END;
-
---rollback DROP PROCEDURE account_count;
+*/
