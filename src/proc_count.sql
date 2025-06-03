6779ed7b719508cca7befe521a299ecba631f37a
lifecracker87 <bhimpdsharma@gmail.com>
2025-06-03 21:19:39 +0545

    Update proc_count.sql

--- src/proc_count.sql
+++ src/proc_count.sql
@@ -1,11 +1,12 @@
 --liquibase formatted sql
 
 --changeset bhim.sharma:100
-CREATE OR REPLACE FUNCTION a_proc()
-RETURNS TABLE(foo TEXT, bar TEXT) AS $$
+CREATE PROCEDURE log_message()
+LANGUAGE plpgsql
+AS $$
 BEGIN
-  RETURN QUERY SELECT 'foo', 'bar';
+  RAISE NOTICE 'This is a sample procedure.';
 END;
-$$ LANGUAGE plpgsql;
+$$;
 
---rollback DROP FUNCTION IF EXISTS a_proc();
+--rollback DROP PROCEDURE IF EXISTS log_message();
