ef4f54d02d42980859e4705864e726a5cc839389
lifecracker87 <bhimpdsharma@gmail.com>
2025-06-04 22:49:58 +0545

    Update proc_aacount_count.sql

--- src/proc_aacount_count.sql
+++ src/proc_aacount_count.sql
@@ -3,7 +3,11 @@
 --changeset bhim.sharma:100 runOnChange:true
 CREATE PROCEDURE account_count()
 BEGIN
-  SELECT 'Number of accounts:', COUNT(*) FROM mysql.user;
+  SELECT 'Number of accounts in DB:', COUNT(*) FROM mysql.user;
 END;
 
 --rollback DROP PROCEDURE account_count;
+--rollback CREATE PROCEDURE account_count()
+--rollback BEGIN
+--rollback SELECT 'Number of accounts:', COUNT(*) FROM mysql.user;
+--rollback END;
