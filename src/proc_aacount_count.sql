b9d71ac0fa9f8ae6bcc7db09a7f9916ece1d8e92
lifecracker87 <bhimpdsharma@gmail.com>
2025-06-04 22:54:37 +0545

    Update proc_aacount_count.sql

--- src/proc_aacount_count.sql
+++ src/proc_aacount_count.sql
@@ -1,13 +1,12 @@
 ﻿--liquibase formatted sql
 
 --changeset bhim.sharma:100 runOnChange:true
-CREATE PROCEDURE account_count()
+CREATE OR REPLACE PROCEDURE account_count()
 BEGIN
   SELECT 'Number of accounts in DB:', COUNT(*) FROM mysql.user;
 END;
 
---rollback DROP PROCEDURE account_count;
---rollback CREATE PROCEDURE account_count()
+--rollback CREATE OR REPLACE PROCEDURE account_count()
 --rollback BEGIN
 --rollback SELECT 'Number of accounts:', COUNT(*) FROM mysql.user;
 --rollback END;
