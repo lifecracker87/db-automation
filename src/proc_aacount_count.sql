937ede1f2f46680a8df0b047675990b6e9b82ea4
lifecracker87 <bhimpdsharma@gmail.com>
2025-06-05 15:29:42 +0545

    Update proc_aacount_count.sql

--- src/proc_aacount_count.sql
+++ src/proc_aacount_count.sql
@@ -5,5 +5,6 @@ CREATE OR REPLACE PROCEDURE greet_user (username IN VARCHAR2) AS
 BEGIN
   DBMS_OUTPUT.PUT_LINE('Hello, ' || username || '!');
 END;
+/
 
 --rollback DROP PROCEDURE IF EXISTS greet_user;
